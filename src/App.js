a904f1c6ac6ea6004faa6ec9ceae64a11255cb07
NiroNithy <nironithy27@gmail.com>
2023-09-12 22:27:53 +0530

    navbar create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Component/Navbar";
 
 function App() {
   return (
     <div className="App">
-        <h1>Hi,I am Nirosana Karunanithy</h1>
+        <Navbar/>
     </div>
+  
   );
 }
 
---
+++ src/Component/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../assets/Logo.png';
+import vector from '../assets/Vector.png';
+import {Link} from 'react-scroll';
+import './navbar.css';
+const Navbar = () => {
+  return (
+    <div className='header'>
+    <nav className='Navbar'>
+        <img src={logo} alt='logo' className='logo'/>
+        <div className='menu'>
+           <Link className='menuItem'>Home</Link>
+           <Link className='menuItem'>About</Link>
+           <Link className='menuItem'>Portfolio</Link>
+           <Link className='menuItem'>Skills</Link>
+        </div>
+        <button className='menubtn'>
+            <img src={vector} alt='' className='menulogo'/>Contact me
+        </button>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
